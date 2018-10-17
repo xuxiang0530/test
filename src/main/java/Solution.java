3af957cd00ed62fbc9ded48dbfde949f7d17d96f
xux <tianyaxx@163.com>
2018-10-18 07:33:25 +0800

    1018

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,4 @@
+import java.awt.font.NumericShaper;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,12 +13,44 @@ public class Solution {
 
     /*
     给定一个int数组nums,寻找里面所有的A,B,C集合,使A + B + C = 0.
-    例如: nums = {-1,0,1,2,-1,-1},
+    例如: nums = {-1,0,2,1,-1,-1},
     结果:[[-1,0,1],[-1,-1,2]]
      */
     public List<List<Integer>> threeSum(int[] nums)
     {
         ArrayList<List<Integer>> outList = new ArrayList<List<Integer>>();
+        //ArrayList<Integer> inIntegerList = new ArrayList<Integer>();
+        ArrayList<Integer> outIntegerList = new ArrayList<Integer>();
+
+
+        for(int i = 0;i < nums.length;i++)
+        {
+            for(int j = i + 1;j < nums.length;j++)
+            {
+                for(int x = j + 1;x < nums.length;x++)
+                {
+                    if(!outIntegerList.contains(i) && !outIntegerList.contains(j) && !outIntegerList.contains(x) )
+                    {
+                        if(nums[i] + nums[j] + nums[x] == 0)
+                        {
+                            outIntegerList.add(i);
+                            outIntegerList.add(j);
+                            outIntegerList.add(x);
+                        }
+                    }
+                }
+            }
+        }
+
+//        for(int i = 0;i< outIntegerList.size();i+= 3)
+//        {
+////            ArrayList<Integer> ol = new ArrayList<Integer>();
+////            ol.add(nums[outIntegerList.get(i)]);
+////            ol.add(nums[outIntegerList.get(i+1)]);
+////            ol.add(nums[outIntegerList.get(i+2)]);
+////
+////            ol.sort();
+//        }
 
         return outList;
     }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,8 @@ public class Test {
         int[] nums = {8,9,88,89,97};
         Solution solu = new Solution();
         String s = solu.largestNumber(nums);
+        int[] nu = {-1,0,2,1,-1,-1};
+        solu.threeSum(nu);
         System.out.print(s);
     }
 
